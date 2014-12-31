cb9a475e0b61f0b077408ea87f0a20c2d50f2915
Caleb Davenport <calebmdavenport@gmail.com>
2014-12-31 10:32:28 -0800

    Load data into cocktails view controller.

--- New Year's Eve/CoctailsTableViewController.swift
+++ New Year's Eve/CoctailsTableViewController.swift
@@ -10,4 +10,61 @@ import UIKit
 
 class CoctailsTableViewController: UITableViewController {
     
+    // MARK: - Properties
+    
+    private let data: NSArray? = {
+        if let url = NSBundle.mainBundle().URLForResource("data", withExtension: "json", subdirectory: "Data") {
+            if let data = NSData(contentsOfURL: url) {
+                return NSJSONSerialization.JSONObjectWithData(data, options: nil, error: nil) as? NSArray
+            }
+        }
+        return nil
+    }()
+    
+    
+    // MARK: - Private
+    
+    private func itemsInSection(section: Int) -> NSArray? {
+        if let value = data {
+            if let dictionary = value[section] as? NSDictionary {
+                return dictionary["items"] as? NSArray
+            }
+        }
+        return nil
+    }
+    
+    private func cocktailAtIndexPath(indexPath: NSIndexPath) -> NSDictionary? {
+        if let items = itemsInSection(indexPath.section) {
+            return items[indexPath.row] as? NSDictionary
+        }
+        return nil
+    }
+    
+    
+    // MARK: - UITableViewDataSource
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return data?.count ?? 0
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemsInSection(section)?.count ?? 0
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
+        let cocktail = cocktailAtIndexPath(indexPath)
+        cell.textLabel?.text = cocktail?["title"] as? String
+        cell.detailTextLabel?.text = cocktail?["subtitle"] as? String
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if let value = data {
+            if let dictionary = value[section] as? NSDictionary {
+                return dictionary["title"] as? String
+            }
+        }
+        return nil
+    }
 }
