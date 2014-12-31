0ad7badb5a2156e8a4b3ccd63eb57e2ebd552ee3
Caleb Davenport <calebmdavenport@gmail.com>
2014-12-31 09:58:32 -0800

    Begin laying out app structure.

---
+++ New Year's Eve/SpiritsTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SpiritsTableViewController.swift
+//  New Year's Eve
+//
+//  Created by Caleb Davenport on 12/31/14.
+//  Copyright (c) 2014 Sam Soffes. All rights reserved.
+//
+
+import UIKit
+
+class SpiritsTableViewController: UITableViewController {
+    
+}
