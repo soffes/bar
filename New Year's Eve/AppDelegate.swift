30b84055bcf8b05920c3a680cb9870e6e4abf8ed
Caleb Davenport <calebmdavenport@gmail.com>
2014-12-31 13:23:47 -0800

    Fix app delegate.

--- New Year's Eve/AppDelegate.swift
+++ New Year's Eve/AppDelegate.swift
@@ -43,19 +43,17 @@ import UIKit
 	}
 
 
-	// MARK: - Split view
-
-	func splitViewController(splitViewController: UISplitViewController, collapseSecondaryViewController secondaryViewController:UIViewController!, ontoPrimaryViewController primaryViewController:UIViewController!) -> Bool {
-	    if let secondaryAsNavController = secondaryViewController as? UINavigationController {
-	        if let topAsDetailController = secondaryAsNavController.topViewController as? DetailViewController {
-	            if topAsDetailController.detailItem == nil {
-	                // Return true to indicate that we have handled the collapse by doing nothing; the secondary controller will be discarded.
-	                return true
-	            }
-	        }
-	    }
-	    return false
+	// MARK: - UISplitViewControllerDelegate
+    
+	func splitViewController(splitViewController: UISplitViewController, collapseSecondaryViewController secondaryViewController:UIViewController?, ontoPrimaryViewController primaryViewController:UIViewController?) -> Bool {
+        if let secondaryNavigationController = secondaryViewController as? UINavigationController {
+            if let detailViewController = secondaryNavigationController.topViewController as? CocktailViewController {
+                if detailViewController.cocktail != nil {
+                    return false
+                }
+            }
+        }
+	    return true
 	}
-
 }
 
