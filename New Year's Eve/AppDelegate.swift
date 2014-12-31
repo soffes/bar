1c014d9ce061aaf5f73288d6a0eacdd64b719d91
Caleb Davenport <calebmdavenport@gmail.com>
2014-12-31 12:37:59 -0800

    Clean up app delegate and set title text attributes.

--- New Year's Eve/AppDelegate.swift
+++ New Year's Eve/AppDelegate.swift
@@ -8,42 +8,40 @@
 
 import UIKit
 
-@UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDelegate {
+@UIApplicationMain class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDelegate {
 
+    // MARK: - Properties
+    
 	var window: UIWindow?
+    
+    
+    // MARK: - Private
+    
+    private func configureAppearance() {
+        UINavigationBar.appearance().titleTextAttributes = [
+            NSFontAttributeName: UIFont(name: "Georgia", size: 18)!,
+            NSForegroundColorAttributeName: UIColor.blackColor()
+        ]
+    }
+    
+    private func configureSplitViewController() {
+        if let splitViewController = window?.rootViewController as? UISplitViewController {
+            splitViewController.delegate = self
+            if let navigationController = splitViewController.viewControllers.last as? UINavigationController {
+                navigationController.topViewController?.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem()
+            }
+        }
+    }
 
+    
+    // MARK: - UIApplicationDelegate
 
-	func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-		// Override point for customization after application launch.
-		let splitViewController = self.window!.rootViewController as UISplitViewController
-		let navigationController = splitViewController.viewControllers[splitViewController.viewControllers.count-1] as UINavigationController
-		navigationController.topViewController.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem()
-		splitViewController.delegate = self
+	func application(application: UIApplication, didFinishLaunchingWithOptions options: [NSObject: AnyObject]?) -> Bool {
+        configureAppearance()
+        configureSplitViewController()
 		return true
 	}
 
-	func applicationWillResignActive(application: UIApplication) {
-		// Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-		// Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-	}
-
-	func applicationDidEnterBackground(application: UIApplication) {
-		// Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-		// If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-	}
-
-	func applicationWillEnterForeground(application: UIApplication) {
-		// Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-	}
-
-	func applicationDidBecomeActive(application: UIApplication) {
-		// Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-	}
-
-	func applicationWillTerminate(application: UIApplication) {
-		// Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-	}
 
 	// MARK: - Split view
 
